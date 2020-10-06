db6f1b08eebef17c08626bca6a881ed1cb9bc243
mattsirota <42943764+mattsirota@users.noreply.github.com>
2020-10-06 16:36:03 -0400

    updated test cases with mock

--- hw04a.py
+++ hw04a.py
@@ -1,9 +1,12 @@
 #Matt Sirota
-#SSW-567 HW04a
+#SSW-567 HW05a
 
 import unittest
 import requests
 import json
+from unittest import mock
+
+mock_results = ["Input is Invalid", "User does not exist", {"csp":2, "hellogitworld":30, "helloworld":6, "Mocks":10, "Project1":2, "richkempinski.github.io":9, "threads-of-life":1, "try_nbdev":2, "try_nbdev2":5}]
 
 def getGitHubInfo(userID):
     if (userID == None):
@@ -25,12 +28,24 @@ def getGitHubInfo(userID):
     return repoDict
 
 class TestGitHubAPI(unittest.TestCase):
-    def testInvalidInput(self):
+    maxDiff = None
+
+    @mock.patch(__name__+'.getGitHubInfo', return_value = mock_results[0])
+    @mock.patch('requests.get')
+    def testInvalidInput(self, mock_result, mock_request):
         self.assertEqual(getGitHubInfo(None), "Input is Invalid")
-    def testInvalidID(self):
+    
+    @mock.patch(__name__+'.getGitHubInfo', return_value = mock_results[1])
+    @mock.patch('requests.get')
+    def testInvalidID(self, mock_result, mock_request):
+        mock_request.return_value = mock_results[1]
         self.assertEqual(getGitHubInfo("ThisIsAnInvalidID123456789"), "User does not exist")
-    def testRichkempinkskiKeysAndValues(self):
-        self.assertEqual(getGitHubInfo("richkempinski"), {"csp":2, "hellogitworld":30, "helloworld":6, "Mocks":10, "Project1":2, "richkempinski.github.io":9, "threads-of-life":1, "try_nbdev":2, "try_nbdev2":5})
+
+    @mock.patch(__name__+'.getGitHubInfo', return_value = mock_results[2])
+    @mock.patch('requests.get')
+    def testRichkempinkskiKeysAndValues(self, mock_result, mock_request):
+        mock_request.return_value = mock_results[2]
+        self.assertEqual(getGitHubInfo("richkempinkski"), mock_results[2])
 
 if __name__ == '__main__':
     unittest.main(exit=True)
